f1e13fd16f062063b095ab864929e451c5d3508e
Stiven <stiven@outlook.com>
2016-08-23 15:55:08 -0300

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,12 +1,12 @@
- $rootDir = $env:APPVEYOR_BUILD_FOLDER
+$rootDir = $env:APPVEYOR_BUILD_FOLDER
  $buildNumber = $env:APPVEYOR_BUILD_NUMBER
  $solutionFile = "$rootDir\src\TITcs.SharePoint.SSOM.sln"
  $srcDir = "$rootDir\src\nuget\TITcs.SharePoint.SSOM"
  $slns = ls "$rootDir\src\*.sln"
  $packagesDir = "$rootDir\src\packages"
  $nuspecPath = "$rootDir\src\nuget\TITcs.SharePoint.SSOM\TITcs.SharePoint.SSOM.nuspec"
- $nugetExe = "$rootDir\src\.nuget\NuGet.exe"
- $nupkgPath = "$rootDir\src\NuGet\TITcs.SharePoint.SSOM\TITcs.SharePoint.SSOM.{0}.nupkg"
+ $nugetExe = "$packagesDir\NuGet.CommandLine.2.8.5\tools\NuGet.exe"
+ $nupkgPath = "$rootDir\src\NuGet\TITcs.SharePoint.SSOM\TITcs.SharePoint.{0}.nupkg"
 
 foreach($sln in $slns) {
    nuget restore $sln
