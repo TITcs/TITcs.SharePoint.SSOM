a50aef7c5e2ea127fa2b6544c72ffb0dc7b5b601
stiven.camara <stiven.camara@TIT-WKS-02>
2016-09-06 17:17:47 -0300

    TotalItems for pagging

--- src/TITcs.SharePoint.SSOM/SharePointPagedData.cs
+++ src/TITcs.SharePoint.SSOM/SharePointPagedData.cs
@@ -4,12 +4,15 @@ namespace TITcs.SharePoint.SSOM
 {
     public class SharePointPagedData<TEntity>
     {
-        public SharePointPagedData(ICollection<TEntity> data, string lastPosition)
+        public SharePointPagedData(ICollection<TEntity> data, string lastPosition, int totalItens)
         {
             LastPosition = lastPosition;
-            Data = data;  
+            Data = data;
+            TotalItems = totalItens;
         }
         public string LastPosition { get; set; }
+
+        public int TotalItems { get; set; }
 
         public ICollection<TEntity> Data { get; set; }
     }
