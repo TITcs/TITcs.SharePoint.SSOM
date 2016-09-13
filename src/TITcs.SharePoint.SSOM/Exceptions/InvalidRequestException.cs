aad2f93948cc5e69c1b3b61f983bc821c97131c4
stiven.camara <stiven.camara@TIT-WKS-02>
2016-09-13 14:06:52 -0300

    Exception

---
+++ src/TITcs.SharePoint.SSOM/Exceptions/InvalidRequestException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TITcs.SharePoint.SSOM.Exceptions
+{
+    public class InvalidRequestException : Exception
+    {
+        public InvalidRequestException() : base("Tentativa de requisição inválida")
+        {
+        }
+
+        public InvalidRequestException(string message) : base(string.Format("Tentativa de requisição inválida. {0}", message))
+        {
+        }
+    }
+}
