5b7ddf90bc4731a8472e88dde104a2211d559b17
stiven.camara <stiven.camara@TIT-WKS-02>
2016-09-14 13:50:23 -0300

    ISharePointRepository

---
+++ src/TITcs.SharePoint.SSOM/ISharePointRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace TITcs.SharePoint.SSOM
+{
+    public interface ISharePointRepository<TEntity> where TEntity : class
+    {
+        uint RowLimit { get; set; }
+        string Title { get; set; }
+        TEntity GetById(int id);
+        SharePointPagedData<TEntity> GetAll(string lastPosition, string camlQuery = null);
+        ICollection<TEntity> GetAll(string camlQuery = null);
+    }
+}
