ebf4c7e861505cc3b489a36b62340d988ccc3201
stiven.camara <stiven.camara@TIT-WKS-02>
2016-09-22 15:48:52 -0300

    AD Search

---
+++ src/TITcs.SharePoint.SSOM.Test/SearchUserTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.DirectoryServices.AccountManagement;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TITcs.SharePoint.SSOM.ActiveDirectory;
+
+namespace TITcs.SharePoint.SSOM.Test
+{
+    /// <summary>
+    /// Summary description for SearchUser
+    /// </summary>
+    [TestClass]
+    public class SearchUserTest
+    {
+        [TestMethod]
+        public void GetUser()
+        {
+            var search = new Search(new []
+            {
+                new PrincipalContext(ContextType.Domain, "df.titcs.local", "OU=Funcionários,OU=Usuários,DC=df,DC=titcs,DC=local", "devsp.admin", "P@ssw0rd4Dev") 
+            });
+
+
+            var user = search.GetUser("stiven.camara");
+
+            Assert.IsTrue(user != null);
+        }
+    }
+}
---
+++ src/TITcs.SharePoint.SSOM/ActiveDirectory/SearchUser.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.DirectoryServices.AccountManagement;
+using System.Linq;
+
+namespace TITcs.SharePoint.SSOM.ActiveDirectory
+{
+    public class Search
+    {
+        private PrincipalContext[] _contexts { get; set; }
+
+        public Search(PrincipalContext[] contexts)
+        {
+            _contexts = contexts;
+        }
+
+        public User GetUser(string loginName)
+        {
+            foreach (var context in _contexts)
+            {
+                using (UserPrincipal userPrincipal = new UserPrincipal(context))
+                {
+                    if (!string.IsNullOrEmpty(loginName))
+                        userPrincipal.SamAccountName = loginName;
+
+                    var principalSearcher = new PrincipalSearcher(userPrincipal);
+
+                    foreach (var found in principalSearcher.FindAll())
+                    {
+                        if (found is UserPrincipal)
+                        {
+                            var foundUserPrincipal = (UserPrincipal) found;
+
+                            var user = new User()
+                            {
+                                Id = foundUserPrincipal.Guid.ToString(),
+                                Email = foundUserPrincipal.EmailAddress,
+                                Name = foundUserPrincipal.Name,
+                                Login = foundUserPrincipal.SamAccountName,
+                                Groups = foundUserPrincipal.GetAuthorizationGroups().Select(i => new Group()
+                                {
+                                    Name = i.Name,
+                                    Id = i.Guid.ToString()
+                                }).ToArray()
+                            };
+
+                            return user;
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        public Group GetGroup(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
