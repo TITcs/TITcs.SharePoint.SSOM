0cf8b998ae2c2510985b0094534d537002d9fc4c
stiven.camara <stiven.camara@TIT-WKS-02>
2016-11-18 09:33:36 -0200

    Ajusts Logger

--- src/TITcs.SharePoint.SSOM/Services/ServiceBase.cs
+++ src/TITcs.SharePoint.SSOM/Services/ServiceBase.cs
@@ -37,6 +37,10 @@ namespace TITcs.SharePoint.SSOM.Services
 
         public void ProcessRequest(HttpContext context)
         {
+#if DEBUG
+            Logger.Logger.Debug("ServiceBase.ProcessRequest", "Begin");
+#endif
+
             _httpContext = context;
             _isPost = context.Request.RequestType.Equals("POST");
 
@@ -56,12 +60,12 @@ namespace TITcs.SharePoint.SSOM.Services
             catch (Exception e)
             {
                 Logger.Logger.Unexpected("ServiceBase.ProcessRequest", e.Message);
-#if DEBUG
-                if(e.InnerException != null)
+
+                if (e.InnerException != null)
                 {
                     Logger.Logger.Unexpected("ServiceBase.ProcessRequest.InnerException", e.InnerException.Message);
                 }
-#endif
+
 
                 context.Response.StatusCode = 500;
                 context.Response.TrySkipIisCustomErrors = true;
@@ -70,6 +74,10 @@ namespace TITcs.SharePoint.SSOM.Services
             }
 
             context.Response.Write(JsonConvert.SerializeObject(result));
+
+#if DEBUG
+            Logger.Logger.Debug("ServiceBase.ProcessRequest", "End");
+#endif
         }
 
         private dynamic CreateModel(HttpContext context)
